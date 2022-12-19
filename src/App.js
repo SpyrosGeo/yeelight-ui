bc174efb65a4faeaca7bdd1fbec6204b4bf45a12
SpyrosGeo <xarodasimba@gmail.com>
2022-12-19 17:17:24 +0200

    continue refactoring

--- src/App.js
+++ src/App.js
@@ -6,20 +6,18 @@ import './App.css';
 
 function App() {
   const BASE_URL='https://yl-api.thatguy.gr'
-  const [powerState, setPowerState] = useState('')
+  const [powerState, setPowerState] = useState({device:'',power:''})
   const [lightColor, setLightColor] = useState('')
   const [bulb, setBulb] = useState('')
   const [strip, setStrip] = useState('')
   const [devices,setDevices]=useState('')
-
+  const [tapo,setTapo]=useState('')
   useEffect(() => {
     const fetchData = async () => {
-      const response = await axios.get(BASE_URL)
-      const { color, power } = response.data
-      power ==="on"?setDevices('off'):setDevices('on')
-      setPowerState(power)
-      setLightColor(color)
-
+      
+      const response = await axios.get(`${BASE_URL}/devices`)
+      setBulb(response.data.bulb.power)
+      setStrip(response.data.strip.power)
     }
     fetchData()
   }, [powerState])
@@ -27,13 +25,13 @@ function App() {
   const handleSubmit = async (e,device) => {
     e.preventDefault()
     const {data} = await axios.get(`${BASE_URL}/${device}`)
-    if(device==='bulb')setBulb(data.power==="on"?'off':'on')
-    if(device==='strip')setBulb(data.power==="on"?'off':'on')
-    setPowerState('')
+    if(device==='bulb'){setBulb(data.power==="on"?'off':'on')}
+    if(device==='strip')setStrip(data.power==="on"?'off':'on')
+    setPowerState({device,power:data.power})
+    console.log('powerState',powerState)
   }
 
   const handleColorPickerChange = (color) => {
-    // setColorPicker(color);
     let hexColor = color.hex
     if (hexColor.length > 5) {
       hexColor = hexColor.substring(1)
@@ -43,18 +41,13 @@ function App() {
     }
   }
 
-  const handleDefaultLightState = () => {
-    const response = axios.get(`${BASE_URL}/default`)
-    setLightColor('#ccd613')
-  }
+  
   return (
-    <div className="App">
-      <h1 style={powerState === "on" ? { color: `#${lightColor}` } : { color: 'red' }}>Bulb is {powerState}</h1>
-      <div className="button-group">
-        <button className="btn" onClick={e=>handleSubmit(e,'on')}> Turn Lights {devices}</button>
-        <button className="btn" onClick={e=>handleSubmit(e,'bulb')}> Turn Bulb {bulb}</button>
-        <button className="btn" onClick={e=>handleSubmit(e,'strip')}> Turn Strip {strip}</button>
-      <button className="btn" onClick={handleDefaultLightState}>Default Light</button>
+    <div className="App ">
+      <div className="container button-group">
+        <button className="btn"style={{backgroundColor:bulb==="off"?"#eb3d30":"#008e81"}} onClick={e=>handleSubmit(e,'bulb')}> Turn Bulb {bulb}</button>
+        <button className="btn"style={{backgroundColor:strip==="off"?"#eb3d30":"#008e81"}} onClick={e=>handleSubmit(e,'strip')}> Turn Strip {strip}</button>
+        <button className="btn" onClick={e=>handleSubmit(e,'default')}>Default Light</button>
       </div>
 
       <TwitterPicker
