ca7a1e999f30655f150202a093bf795a9c38f324
Spyros Georgiadis <s.georgiadis@pmmretail.com>
2022-12-29 22:34:31 +0200

    started implementing brightness

--- src/App.js
+++ src/App.js
@@ -1,67 +1,105 @@
-import axios from 'axios';
-import { useState, useEffect } from 'react';
-import {Button} from 'react-bootstrap';
-import { TwitterPicker } from 'react-color'
-import DeviceCard from './DeviceCard';
-import './App.css';
+import axios from "axios";
+import { useState, useEffect } from "react";
+import { Button } from "react-bootstrap";
+import { TwitterPicker } from "react-color";
+import DeviceCard from "./DeviceCard";
+import "./App.css";
 function App() {
-  const BASE_URL='https://yl-api.thatguy.gr'
-  const [powerState, setPowerState] = useState({device:'',power:''})
-  const [lightColor, setLightColor] = useState('')
-  const [bulb, setBulb] = useState({})
-  const [strip, setStrip] = useState({})
-  const [devices,setDevices]=useState('')
-  const [tapo,setTapo]=useState('')
+  const BASE_URL = "https://yl-api.thatguy.gr";
+  const [powerState, setPowerState] = useState({ device: "", power: "" });
+  const [lightColor, setLightColor] = useState("");
+
+  const [bulbBrightness, setBulbBrightness] = useState("");
+  const [bulb, setBulb] = useState({});
+  const [strip, setStrip] = useState({});
+  const [tapo, setTapo] = useState("");
   useEffect(() => {
     const fetchData = async () => {
-      
-      const response = await axios.get(`${BASE_URL}/devices`)
-      console.log('response',response.data.tapo)
-      response.data.bulb.name='bulb'
-      response.data.strip.name='strip'
-      response.data.tapo.name='tapo'
-      setBulb(response.data.bulb)
-      setStrip(response.data.strip)
-      setTapo(response.data.tapo)
-    }
-    fetchData()
-  }, [powerState])
+      const response = await axios.get(`${BASE_URL}/devices`);
+      console.log("response", response.data.tapo);
+      response.data.bulb.name = "bulb";
+      response.data.strip.name = "strip";
+      response.data.tapo.name = "tapo";
+      setBulb(response.data.bulb);
+      setStrip(response.data.strip);
+      setTapo(response.data.tapo);
+    };
+    fetchData();
+  }, [powerState]);
 
-  const handleSubmit = async (e,device) => {
-    e.preventDefault()
-    const {data} = await axios.get(`${BASE_URL}/${device}`)
-    if(device==='bulb'){setBulb(data.power==="on"?'off':'on')}
-    if(device==='strip')setStrip(data.power==="on"?'off':'on')
-    if(device ==='tapo')setTapo(data.power ==="on"?'off':'on')
-    setPowerState({device,power:data.power})
-  }
+  const handleSubmit = async (e, device) => {
+    e.preventDefault();
+    const { data } = await axios.get(`${BASE_URL}/${device}`);
+    if (device === "bulb") {
+      setBulb(data.power === "on" ? "off" : "on");
+    }
+    if (device === "strip") setStrip(data.power === "on" ? "off" : "on");
+    if (device === "tapo") setTapo(data.power === "on" ? "off" : "on");
+    setPowerState({ device, power: data.power });
+  };
+  const handleBrightness = (device, brightness) => {
+    console.log("device", device);
+  };
 
   const handleColorPickerChange = (color) => {
-    let hexColor = color.hex
+    let hexColor = color.hex;
     if (hexColor.length > 5) {
-      hexColor = hexColor.substring(1)
-      setLightColor(hexColor)
-      const response = axios.get(`${BASE_URL}/color/${hexColor}`)
+      hexColor = hexColor.substring(1);
+      setLightColor(hexColor);
+      const response = axios.get(`${BASE_URL}/color/${hexColor}`);
     }
-  }
+  };
 
-  
   return (
     <div className="App container ">
       <div className="row d-flex justify-content-center my-5 ">
-      <TwitterPicker
-        color={['#f7f379', '#f7f379', '#f7f379', '#f7f379', '#f7f379', '#f7f379', '#f7f379', '#f7f379', "#f7f178", "#f7f178", "#f7f178"]}
-        triangle={"hide"}
-        onChange={handleColorPickerChange} />
-        <Button  className='mt-2 col col-9 shadow' variant="warning" onClick={e=>(handleSubmit(e,'default'))}>Default</Button>
+        <TwitterPicker
+          color={[
+            "#f7f379",
+            "#f7f379",
+            "#f7f379",
+            "#f7f379",
+            "#f7f379",
+            "#f7f379",
+            "#f7f379",
+            "#f7f379",
+            "#f7f178",
+            "#f7f178",
+            "#f7f178",
+          ]}
+          triangle={"hide"}
+          onChange={handleColorPickerChange}
+        />
+        <Button
+          className="mt-2 col col-9 shadow"
+          variant="warning"
+          onClick={(e) => handleSubmit(e, "default")}
+        >
+          Default
+        </Button>
       </div>
-      <div className=' container d-flex justify-content-around mt-5'>
-      <DeviceCard  device={bulb} handleSubmit={handleSubmit} handleColorPickerChange={handleColorPickerChange}/>
-       <DeviceCard  device={tapo} handleSubmit={handleSubmit} handleColorPickerChange={handleColorPickerChange}/>
-      <DeviceCard  device={strip} handleSubmit={handleSubmit} handleColorPickerChange={handleColorPickerChange}/>
-        </div>
+      <div className=" container d-flex justify-content-around mt-5">
+        <DeviceCard
+          device={bulb}
+          brightness={bulbBrightness}
+          handleBrightness={handleBrightness}
+          handleSubmit={handleSubmit}
+          handleColorPickerChange={handleColorPickerChange}
+        />
+        <DeviceCard
+          device={tapo}
+          handleBrightness={handleBrightness}
+          handleSubmit={handleSubmit}
+          handleColorPickerChange={handleColorPickerChange}
+        />
+        <DeviceCard
+          device={strip}
+          handleBrightness={handleBrightness}
+          handleSubmit={handleSubmit}
+          handleColorPickerChange={handleColorPickerChange}
+        />
+      </div>
     </div>
-
   );
 }
 
--- src/DeviceCard.jsx
+++ src/DeviceCard.jsx
@@ -1,14 +1,54 @@
-import {Card,Button, Image} from "react-bootstrap"
-import { TwitterPicker } from "react-color"
-const DeviceCard = ({device,handleSubmit,handleColorPickerChange})=>{
-  return (
-    <Card className="shadow" onClick={e=>handleSubmit(e,device.name)} text="white" bg={device.power==='off'?'danger':'success'} style={{ width: '11rem' }}>
-      <Card.Img style={{height:'13rem'}}  as={Image}variant="top" src={`${device.name?.toLowerCase()}.jpg`} />
-      <Card.Body>
-    
-      </Card.Body>
-    </Card>
-  )
+import { useState } from "react";
+import { Button, Card, Image } from "react-bootstrap";
+import { TwitterPicker } from "react-color";
+const DeviceCard = ({
+  device,
+  brightness,
+  handleSubmit,
+  handleColorPickerChange,
+  handleBrightness,
+}) => {
+  const [slider, setSlider] = useState(0);
 
-}
-export default DeviceCard
+  const handleSlider = (command) => {
+    if(command==="increment" && slider<100){
+    setSlider(slider+10);
+    }
+    if(command==="decrement" && slider>0){
+    setSlider(slider-10);
+    }
+    handleBrightness(device, slider);
+  };
+
+  return (
+    <div>
+      <Card
+        className="shadow"
+        text="white"
+        bg={device.power === "off" ? "danger" : "success"}
+        style={{ width: "11rem" }}
+      >
+        <Card.Img
+          onClick={(e) => handleSubmit(e, device.name)}
+          style={{ height: "13rem" }}
+          as={Image}
+          variant="top"
+          src={`${device.name?.toLowerCase()}.jpg`}
+        />
+        <Card.Body>
+          <div>
+            <label  for="customRange1" class="form-label">
+              Brightness{" "}
+            </label>
+            <div className="d-flex justify-content-between">
+              <Button onClick={()=>{handleSlider('decrement')}}>-</Button>
+              <span>{slider}%</span>
+              <Button onClick={()=>{handleSlider('increment')}}>+</Button>
+            </div>
+          </div>
+        </Card.Body>
+      </Card>
+    </div>
+  );
+};
+export default DeviceCard;
